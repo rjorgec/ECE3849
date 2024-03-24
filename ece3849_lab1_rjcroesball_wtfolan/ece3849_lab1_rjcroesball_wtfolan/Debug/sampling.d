a6d977a3fa7cad1a645467452cc132507298242f
rjorgec <105024606+rjorgec@users.noreply.github.com>
2024-03-24 18:54:55 -0400

    Add files via upload

---
+++ ece3849_lab1_rjcroesball_wtfolan/ece3849_lab1_rjcroesball_wtfolan/Debug/sampling.d
@@ -0,0 +1,75 @@
+# FIXED
+
+sampling.obj: ../sampling.c
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/math.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_defs.h
+sampling.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_limits.h
+sampling.obj: C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_memmap.h
+sampling.obj: C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/sysctl.h
+sampling.obj: C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/gpio.h
+sampling.obj: C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/timer.h
+sampling.obj: C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/interrupt.h
+sampling.obj: ../sysctl_pll.h
+sampling.obj: C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/adc.h
+sampling.obj: C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/tm4c1294ncpdt.h
+sampling.obj: ../buttons.h
+
+../sampling.c:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/math.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_defs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_limits.h:
+
+C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_memmap.h:
+
+C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/sysctl.h:
+
+C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/gpio.h:
+
+C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/timer.h:
+
+C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/interrupt.h:
+
+../sysctl_pll.h:
+
+C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/adc.h:
+
+C:/ti/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/tm4c1294ncpdt.h:
+
+../buttons.h:
+
---
+++ ece3849_lab1_rjcroesball_wtfolan/ece3849_lab1_rjcroesball_wtfolan/sampling.c
@@ -0,0 +1,77 @@
+/*
+ * sampling.c
+ * created 3/21/2024 by Ricardo Croes-Ball and Will-i-am Folan
+ * >:(
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+#include "sysctl_pll.h"
+#include "driverlib/adc.h"
+#include "inc/tm4c1294ncpdt.h"
+#include "buttons.h"
+
+#define ADC_BUFFER_SIZE 2048 // size must be a power of 2
+// index wrapping macro
+#define ADC_BUFFER_WRAP(i) ((i) & (ADC_BUFFER_SIZE - 1))
+// latest sample index
+volatile int32_t gADCBufferIndex = ADC_BUFFER_SIZE - 1;
+volatile uint16_t gADCBuffer[ADC_BUFFER_SIZE]; // circular buffer
+volatile uint32_t gADCErrors = 0; // number of missed ADC deadlines
+
+void ADCInit(void){
+//all the ADC1 stuff
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+  GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_0); // GPIO setup for analog input AIN3
+  // initialize ADC peripherals
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC1);
+  // ADC clock
+    uint32_t pll_frequency = SysCtlFrequencyGet(CRYSTAL_FREQUENCY);
+    uint32_t pll_divisor = (pll_frequency - 1) / (16 * ADC_SAMPLING_RATE) + 1; // round up
+  ADCClockConfigSet(ADC0_BASE, ADC_CLOCK_SRC_PLL | ADC_CLOCK_RATE_FULL, pll_divisor);
+  ADCClockConfigSet(ADC1_BASE, ADC_CLOCK_SRC_PLL | ADC_CLOCK_RATE_FULL, pll_divisor);
+  // choose ADC1 sequence 0; disable before configuring
+  ADCSequenceDisable(ADC1_BASE, 0);
+
+  ADCSequenceConfigure(ADC1_BASE, 0, ADC_TRIGGER_ALWAYS, 0); // specify the "Always" trigger
+
+  // in the 0th step, sample channel 3 (AIN3)
+  // enable interrupt, and make it the end of sequence
+  ADCSequenceStepConfigure(ADC1_BASE, 0, 0, ADC_CTL_CH3 | ADC_CTL_IE | ADC_CTL_END); //Not sure if CH3 is correct, IF SOMETHING DOESNT WORK LOOK HERE
+
+  // enable the sequence. it is now sampling
+  ADCSequenceEnable(ADC1_BASE, 0);
+  // enable sequence 0 interrupt in the ADC1 peripheral
+  ADCIntEnable(ADC1_BASE, 0);
+  IntPrioritySet(INT_ADC1SS0, 0x00); // set ADC1 sequence 0 interrupt priority
+  // enable ADC1 sequence 0 interrupt in int. controller
+  IntEnable(INT_ADC1SS0);
+}
+
+
+
+
+void ADC_ISR(void){
+    // clear ADC1 sequence0 interrupt flag in the ADCISC register
+    ADC1_ISC_R = ADC_ISC_IN0;
+    // check for ADC FIFO overflow
+    if(ADC1_OSTAT_R & ADC_OSTAT_OV0) {
+        gADCErrors++; // count errors
+        ADC1_OSTAT_R = ADC_OSTAT_OV0; // clear overflow condition
+    }
+    gADCBufferIndex = ADC_BUFFER_WRAP(gADCBufferIndex + 1);
+    // read sample from the ADC1 sequence 0 FIFO
+    gADCBuffer[gADCBufferIndex] = ADC1_SSFIFO0_R;
+    return gADCBuffer;
+}
+
+uint16_t ADCSAMPLER(int i){
+    return gADCBuffer[i];
+}
+
